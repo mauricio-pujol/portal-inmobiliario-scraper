0a6bbb255bb64c4af8aabf78c7b9ba59b7e63308
MauricioPujol <mauricio.pujolh@gmail.com>
2024-02-10 00:02:21 -0300

    fix: map plotter as function

--- main.py
+++ main.py
@@ -12,54 +12,61 @@ import os
 from utils import *
 from fetch_property_data import * 
 from locations import locations_coordinates
+from map_plotter import generate_search_grid_points
 
 location = 'renaca'
 print('Iniciando busqueda de propiedades en',location)
-try:
-    url = generate_location_url(locations_coordinates[location][0],locations_coordinates[location][1])
-except Exception as e:
-    print(f"Error: {e}")
-search_details = get_url_details(url)
-set_of_urls = set()
-next_page_url = True
-i = 0
-while next_page_url:
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    enlaces = soup.find_all('a', class_='ui-search-result__image ui-search-link', href=True)
-    for link in enlaces:
-        set_of_urls.add(link.get('href'))
-    next_page = soup.find_all('a', class_='andes-pagination__link', href=True)
-    if soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href') != '':
-        url = soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href')
-    else:
-        next_page_url = False
-    i+=1
-    print('Propiedades encontradas hasta ahora:',len(set_of_urls),'Páginas revisadas:',i)
+start_point, end_point = locations_coordinates[location]
+grid_points = generate_search_grid_points(start_point, end_point)
+n = 1
+for cell in grid_points:
+    print('Escaneando celda:',n)
+    url = generate_location_url(cell[0],cell[2])
+    search_details = get_url_details(url)
+    set_of_urls = set()
+    next_page_url = True
+    i = 0
+    while next_page_url:
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        enlaces = soup.find_all('a', class_='ui-search-result__image ui-search-link', href=True)
+        for link in enlaces:
+            set_of_urls.add(link.get('href'))
+        next_page = soup.find_all('a', class_='andes-pagination__link', href=True)
+        try:
+            if soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href') != '':
+                url = soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href')
+            else:
+                next_page_url = False
+        except:
+            next_page_url = False
+        i+=1
+        print('Se revisaron',i,'páginas en el área de la celda',n,'y se encontraron',len(set_of_urls))
 
-set_of_urls = list(set_of_urls)
-print('Todas las páginas escaneadas con propiedades en',location)
+    set_of_urls = list(set_of_urls)
+    print('Todas las páginas escaneadas con propiedades en',location)
 
-raw_columns = [
-    'url', 'title', 'published', 'price', 'maintenance', 'size', 'bedrooms', 'bathrooms',
-    'broker', 'adress', 'google_maps_pin', 'secondary_attributes', 'description'
-]
+    raw_columns = [
+        'url', 'title','type','published', 'price', 'maintenance', 'size', 'bedrooms', 'bathrooms',
+        'broker', 'adress', 'google_maps_pin', 'secondary_attributes', 'description'
+    ]
+    # Inicializar un DataFrame vacío con las columnas
+    raw_properties_df = pd.DataFrame(columns=raw_columns)
+    for j in len(set_of_urls):
+        print('Propiedad numero:',j,set_of_urls[j])
+        try:
+            raw_properties_df = raw_properties_df.append(extract_property_raw_data(set_of_urls[j]))
+            print('Exito')
+        except:
+            print('Fallido')
+            pass
 
-# Inicializar un DataFrame vacío con las columnas
-raw_properties_df = pd.DataFrame(columns=raw_columns)
-for j in range(30):
-    print('Propiedad numero:',j,set_of_urls[j])
-    try:
-        raw_properties_df = raw_properties_df.append(extract_property_raw_data(set_of_urls[j]))
-        print('Exito')
-    except:
-        print('Fallido')
-        pass
-
-current_time_text = str(datetime.now()).replace(':','.')
-csv_name = search_details['operation']+'-' +search_details['property_type'] +'-'+location+'_'+current_time_text+'.csv'
-path_save = os.path.join('properties_raw_data', csv_name)
-if not os.path.exists('properties_raw_data'):
-    os.makedirs('properties_raw_data')
-raw_properties_df.to_csv(path_save, index=False)
+    current_time_text = str(datetime.now()).replace(':','.')
+    csv_name = search_details['operation']+'-' +search_details['property_type'] +'-'+location+'_'+current_time_text+'.csv'
+    path_save = os.path.join('properties_raw_data', csv_name)
+    if not os.path.exists('properties_raw_data'):
+        os.makedirs('properties_raw_data')
+    raw_properties_df.to_csv(path_save, index=False)
+    n+=1
+    break
--- map_plotter.py
+++ map_plotter.py
@@ -3,93 +3,89 @@ from IPython.display import display
 import numpy as np
 import math
 from utils import calculate_new_position
-# Coordenadas de las esquinas del rectángulo
-search_area_start_coord = list([-32.98643, -71.55181])
-search_area_end_coord = list([-32.95199, -71.52434])
 
-# Calcular las coordenadas de las otras dos esquinas del rectángulo
-area_bottom_left = list([search_area_start_coord[0], search_area_start_coord[1]]) #ok
-area_bottom_right = list([search_area_start_coord[0], search_area_end_coord[1]]) # ok
-area_top_left = list([search_area_end_coord[0], search_area_start_coord[1]])
-area_top_right = list([search_area_end_coord[0], search_area_end_coord[1]]) #ok
-# Calcular el centro del rectángulo
-lat_center = (search_area_start_coord[0] + search_area_end_coord[0]) / 2
-lon_center = (search_area_start_coord[1] + search_area_end_coord[1]) / 2
+def generate_search_grid_points(search_area_start_coord,search_area_end_coord):
+    # Calcular las coordenadas de las otras dos esquinas del rectángulo
+    area_bottom_left = list([search_area_start_coord[0], search_area_start_coord[1]])
+    area_bottom_right = list([search_area_start_coord[0], search_area_end_coord[1]]) 
+    area_top_left = list([search_area_end_coord[0], search_area_start_coord[1]])
+    area_top_right = list([search_area_end_coord[0], search_area_end_coord[1]]) 
 
-# Crear un mapa centrado en el centro del rectángulo
-mapa = folium.Map(location=[lat_center, lon_center], zoom_start=13)
+    # Calcular el centro del rectángulo
+    lat_center = (search_area_start_coord[0] + search_area_end_coord[0]) / 2
+    lon_center = (search_area_start_coord[1] + search_area_end_coord[1]) / 2
+    # Crear un mapa centrado en el centro del rectángulo
+    mapa = folium.Map(location=[lat_center, lon_center], zoom_start=13)
 
-# Agregar el rectángulo al mapa
-search_area = folium.Polygon(
-    locations=[area_bottom_left, area_bottom_right, area_top_right, area_top_left],
-    color='#3498db',  # Bordes en color naranja
-    fill=True,
-    fill_color='#3498db',  # Relleno en color azul claro
-    fill_opacity=0.1
-)
-
-# Añadir el rectángulo al mapa
-search_area.add_to(mapa)
-grid_points = []
-cell_bottom_left= area_bottom_left
-j= 1
-draw_grid = True
-while draw_grid:
-    #796x460
-    cell_top_left = calculate_new_position(cell_bottom_left,460,'north')
-    cell_top_right = calculate_new_position(calculate_new_position(cell_bottom_left,460,'north'),796,'east')
-    cell_bottom_right= calculate_new_position(cell_bottom_left,796,'east')
-    grid_points.append([cell_bottom_left, cell_top_left, cell_top_right,cell_bottom_right,cell_bottom_left])
-    cell_area = folium.Polygon(
-        locations=[cell_bottom_left, cell_top_left, cell_top_right,cell_bottom_right,cell_bottom_left],
-        color='navy',  # Bordes en color naranja
+    # Agregar el rectángulo al mapa
+    search_area = folium.Polygon(
+        locations=[area_bottom_left, area_bottom_right, area_top_right, area_top_left],
+        color='#3498db',  # Bordes en color naranja
         fill=True,
         fill_color='#3498db',  # Relleno en color azul claro
-        fill_opacity=0.2
+        fill_opacity=0.1
     )
-    cell_bottom_left = calculate_new_position(cell_bottom_left,700,'east')
-    if cell_bottom_right[1] >area_bottom_right[1]:#Si se sale del recuadro horizontalmente
-         cell_bottom_left = calculate_new_position(area_bottom_left,400*j,'north')
-         j+=1
-    if cell_top_right[0] > area_top_right[0] and cell_top_right[1] > area_top_right[1]:
-         draw_grid = False
-    cell_area.add_to(mapa)
 
-ajuste_lat = -(grid_points[-1][2][0]-area_top_right[0])/2
-ajuste_lon = -(grid_points[-1][2][1]-area_top_right[1])/2
+    # Añadir el rectángulo al mapa
+    search_area.add_to(mapa)
+    grid_points = []
+    cell_bottom_left= area_bottom_left
+    j= 1
+    draw_grid = True
+    while draw_grid: # El área de busqueda de cada celda es 796mx460m.
+        cell_top_left = calculate_new_position(cell_bottom_left,460,'north')
+        cell_top_right = calculate_new_position(calculate_new_position(cell_bottom_left,460,'north'),796,'east')
+        cell_bottom_right= calculate_new_position(cell_bottom_left,796,'east')
+        grid_points.append([cell_bottom_left, cell_top_left, cell_top_right,cell_bottom_right,cell_bottom_left])
+        cell_area = folium.Polygon(
+            locations=[cell_bottom_left, cell_top_left, cell_top_right,cell_bottom_right,cell_bottom_left],
+            color='navy',  # Bordes en color naranja
+            fill=True,
+            fill_color='#3498db',  # Relleno en color azul claro
+            fill_opacity=0.2
+        )
+        cell_bottom_left = calculate_new_position(cell_bottom_left,700,'east')
+        if cell_bottom_right[1] >area_bottom_right[1]:#Si se sale del recuadro horizontalmente
+            cell_bottom_left = calculate_new_position(area_bottom_left,400*j,'north')
+            j+=1
+        if cell_top_right[0] > area_top_right[0] and cell_top_right[1] > area_top_right[1]: #Si se sale del recuadro horizontal y verticalmente
+            draw_grid = False
+        cell_area.add_to(mapa)
 
-# Mostrar el mapa en la consola
-display(mapa)
+    ajuste_lat = -(grid_points[-1][2][0]-area_top_right[0])/2
+    ajuste_lon = -(grid_points[-1][2][1]-area_top_right[1])/2
 
 
-new_map = folium.Map(location=[lat_center, lon_center], zoom_start=13)
-for cell in grid_points:
-     cell = np.array(cell) 
-     cell[:, 0] += ajuste_lat
-     cell[:, 1] += ajuste_lon
-     cell[0] = cell[-1]
-     cell_area = folium.Polygon(
-        locations=[cell],
-        color='navy',  # Bordes en color naranja
+    new_map = folium.Map(location=[lat_center, lon_center], zoom_start=13)
+    for cell in grid_points:
+        cell = np.array(cell) 
+        cell[:, 0] += ajuste_lat
+        cell[:, 1] += ajuste_lon
+        cell[0] = cell[-1]
+        cell_area = folium.Polygon(
+            locations=[cell],
+            color='navy',  # Bordes en color naranja
+            fill=True,
+            fill_color='#3498db',  # Relleno en color azul claro
+            fill_opacity=0.2
+        )
+        cell_area.add_to(new_map)
+
+
+    search_area = folium.Polygon(
+        locations=[area_bottom_left, area_bottom_right, area_top_right, area_top_left],
+        color='#3498db',  # Bordes en color naranja
         fill=True,
         fill_color='#3498db',  # Relleno en color azul claro
-        fill_opacity=0.2
+        fill_opacity=0.1
     )
-     cell_area.add_to(new_map)
 
+    # Añadir el rectángulo al mapa
+    search_area.add_to(new_map)
+    print('Mostrando mapa generado con',len(grid_points),'celdas. En color celeste es el área de búsqueda original y en azul las celdas en que se busca individualmente.')
+    display(new_map)
+    return(grid_points)
 
-search_area = folium.Polygon(
-    locations=[area_bottom_left, area_bottom_right, area_top_right, area_top_left],
-    color='#3498db',  # Bordes en color naranja
-    fill=True,
-    fill_color='#3498db',  # Relleno en color azul claro
-    fill_opacity=0.1
-)
-
-# Añadir el rectángulo al mapa
-search_area.add_to(new_map)
-
-
-display(new_map)
-
-
+#search_area_start_point= list([-32.98643, -71.55181])
+#search_area_end_point = list([-32.95199, -71.52434])
+#generate_search_grid_points(search_area_start_point,search_area_end_point)
