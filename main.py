5ded93a8da1ca252d9ec9438ccce06b4ccba01c8
MauricioPujol <mauricio.pujolh@gmail.com>
2024-02-11 15:17:19 -0300

    fix: rooms extract fixed

--- main.py
+++ main.py
@@ -11,64 +11,57 @@ import os
 
 from utils import *
 from fetch_property_data import * 
-from locations import locations_coordinates
-from map_plotter import generate_search_grid_points
+from locations import locations_url
+#from map_plotter import generate_search_grid_points
 
-location = 'renaca'
-print('Iniciando busqueda de propiedades en',location)
-start_point, end_point = locations_coordinates[location]
-grid_points = generate_search_grid_points(start_point, end_point)
-n = 1
-for cell in grid_points:
-    print('Escaneando celda:',n)
-    try:
-        url = generate_location_url(cell[0],cell[2])
-        print(url)
-        search_details = get_url_details(url)
-        set_of_urls = set()
-        next_page_url = True
-        i = 0
-        while next_page_url:
-            page = urlopen(url)
-            html = page.read().decode("utf-8")
-            soup = BeautifulSoup(html, "html.parser")
-            enlaces = soup.find_all('a', class_='ui-search-result__image ui-search-link', href=True)
-            for link in enlaces:
-                set_of_urls.add(link.get('href'))
-            next_page = soup.find_all('a', class_='andes-pagination__link', href=True)
-            try:
-                if soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href') != '':
-                    url = soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href')
-                else:
-                    next_page_url = False
-            except:
+location = 'Reñaca'
+url = locations_url[location]
+print('Iniciando busqueda de propiedades en',location,'\n',url)
+try:
+    search_details = get_url_details(url)
+    set_of_urls = set()
+    next_page_url = True
+    i = 0
+    while next_page_url:
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        enlaces = soup.find_all('a', class_='ui-search-result__image ui-search-link', href=True)
+        for link in enlaces:
+            set_of_urls.add(link.get('href'))
+        next_page = soup.find_all('a', class_='andes-pagination__link', href=True)
+        try:
+            if soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href') != '':
+                url = soup.find_all('a', class_='andes-pagination__link', href=True)[1].get('href')
+            else:
                 next_page_url = False
-            i+=1
-            print('La celda',n,'contiene',i,'página/s y',len(set_of_urls),'propiedad/es.')
-
-        set_of_urls = list(set_of_urls)
-
-        raw_columns = [
+        except:
+            next_page_url = False
+        i+=1
+        print('Se han escaneado',i,'página/s con un acumulado de',len(set_of_urls),'propiedades.')
+    total_properties_expected = soup.find('div','ui-search-search-result').span.text
+    print('Total de propiedades esperadas:', total_properties_expected)
+    set_of_urls = list(set_of_urls)
+    raw_columns = [
             'url', 'title','type','published', 'price', 'maintenance', 'size', 'bedrooms', 'bathrooms',
             'broker', 'adress', 'google_maps_pin', 'secondary_attributes', 'description'
         ]
-        # Inicializar un DataFrame vacío con las columnas
-        raw_properties_df = pd.DataFrame(columns=raw_columns)
-        for j in range(len(set_of_urls)):
-            print('Propiedad numero:',j+1,set_of_urls[j])
-            try:
-                raw_properties_df = raw_properties_df.append(extract_property_raw_data(set_of_urls[j]))
-            except:
-                print('Fallido')
-                pass
+    # Inicializar un DataFrame vacío con las columnas
+    raw_properties_df = pd.DataFrame(columns=raw_columns)
+    for j in range(20):
+        print('Propiedad numero:',j+1,set_of_urls[j])
+        try:
+            raw_properties_df = raw_properties_df.append(extract_property_raw_data(set_of_urls[j]))
+        except:
+            print('Fallido')
+            pass
 
-        current_time_text = str(datetime.now()).replace(':','.')
-        csv_name = search_details['operation']+'-' +search_details['property_type'] +'-'+location+'_'+current_time_text+'.csv'
-        path_save = os.path.join('properties_raw_data', csv_name)
-        if not os.path.exists('properties_raw_data'):
-            os.makedirs('properties_raw_data')
-        raw_properties_df.to_csv(path_save, index=False)
-    except:
-        print('No se encontraron propiedades en la celda.')
-        pass
-    n+=1
+    current_time_text = str(datetime.now()).replace(':','.')
+    csv_name = search_details['operation']+'-' +search_details['property_type'] +'-'+location+'_'+current_time_text+'.csv'
+    path_save = os.path.join('properties_raw_data', csv_name)
+    if not os.path.exists('properties_raw_data'):
+        os.makedirs('properties_raw_data')
+    raw_properties_df.to_csv(path_save, index=False)
+except:
+    print('No se pudo completar la busqueda.')
+    pass
--- merge.py
+++ merge.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import os
-
+from utils import *
 directorio = 'properties_raw_data'
 archivos_csv = [archivo for archivo in os.listdir(directorio) if archivo.endswith('.csv')]
 dataframes = []
@@ -12,6 +12,70 @@ for archivo in archivos_csv:
     dataframes.append(df)
 
 # Concatenar los DataFrames en uno solo
-resultado_final = pd.concat(dataframes, ignore_index=True)
+raw_merge_df = pd.concat(dataframes, ignore_index=True)
 
-resultado_final.to_excel('resultado_final.xlsx', index=False)
+#raw_merge_df.to_excel('raw_merge_df.xlsx', index=False)
+
+
+def calculate_days(description):
+    match = re.search(r'(\d+)', description)
+    if match:
+        cantidad = int(match.group())
+        if 'mes' in description:
+            dias= cantidad * 30
+        elif 'año' in description:
+            dias= cantidad * 365
+        elif 'día' in description:
+            dias= cantidad
+    print(description)
+    return dias
+
+def clean_to_uf(price,uf_current_value):
+    if 'UF' in price:
+        return(int(re.sub(r'\D', '', price)))
+    else:
+        return(int(float(re.sub(r'\D', '', price))/uf_current_value))
+
+def clean_to_clp(price,uf_current_value):
+    if 'UF' in price:
+        return(int(float(re.sub(r'\D', '', price))*uf_current_value))
+    else:
+        return(int(float(re.sub(r'\D', '', price))))
+    
+def clean_maintenance(description):
+    if pd.notna(description):
+        match = re.search(r'\b(\d+(\.\d+)?)\b', str(description))
+        return(int(match.group(1).replace('.','')))
+    else:
+        return None
+
+def clean_size(size_input):
+    match = re.search(r'\b(\d+(\.\d+)?)\b', str(size_input))
+    if 'total' in size_input:
+        match = re.search(r'\b(\d+(\.\d+)?)\b', str(size_input))
+        clean_size = float(match.group(1))
+    else:
+        clean_size = None
+    return clean_size
+
+def clean_room(room_input):
+    match = re.search(r'\b\d{1,2}\b', str(room_input))
+    if match:
+        return int(match.group(0))
+    else:
+        return None
+
+
+
+raw_merge_df
+uf_value = get_uf_currente_value()
+
+clean_df = raw_merge_df[['url','title','type','broker','adress']]
+clean_df['published_days'] = raw_merge_df['published'].apply(calculate_days)
+clean_df['price_uf'] = raw_merge_df['price'].apply(clean_to_uf,uf_current_value = uf_value)
+clean_df['price_clp'] = raw_merge_df['price'].apply(clean_to_clp,uf_current_value = uf_value)
+clean_df['maintenance'] = raw_merge_df['maintenance'].apply(clean_maintenance)
+clean_df['size_m2'] = raw_merge_df['size'].apply(clean_size)
+clean_df['bedrooms'] = raw_merge_df['bedrooms'].apply(clean_room)
+clean_df['bathrooms'] = raw_merge_df['bathrooms'].apply(clean_room)
+clean_df
